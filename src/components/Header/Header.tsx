3f79ee832a179976509bb68063999b4c4c986e55
amscrow <arthursakemi@gmail.com>
2021-10-28 22:29:40 -0300

    creates demo header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+import { StyledH1, StyledHeader } from './header.styles';
+
+const Header: React.FC = () => (
+  <StyledHeader>
+    <StyledH1>Next Template</StyledH1>
+  </StyledHeader>
+);
+
+export default Header;
---
+++ src/components/Header/header.styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const StyledHeader = styled.header`
+  padding: 40px;
+`;
+
+export const StyledH1 = styled.h1`
+  text-align: center;
+`;
