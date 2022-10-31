dd16839460c063343e24ae094a56284b7342bb45
Arthur Sakemi <arthursakemi@gmail.com>
2022-10-31 12:10:54 -0300

    feat(utils): adds curry fn (#7)

---
+++ src/common/utils/curry/curry.ts
@@ -0,0 +1,11 @@
+export function curry(fn) {
+  const arity = fn.length;
+
+  return function $curry(...args) {
+    if (args.length < arity) {
+      return $curry.bind(null, ...args);
+    }
+
+    return fn.call(null, ...args);
+  };
+}
